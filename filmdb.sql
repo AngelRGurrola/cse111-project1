750000b35235c28012066d7b5ae5017c37eb0fd7
Angel Gurrola <93462282+AngelRGurrola@users.noreply.github.com>
2023-11-07 13:58:34 -0800

    Add files via upload

--- filmdb.sql
+++ filmdb.sql
@@ -104,25 +104,3 @@ CREATE TABLE tvDetail(
 );
 
 .import "TMDB_tv_dataset_v3.csv" tvDetail
-
-
-
-
-
-INSERT INTO Users VALUES(1001, 'Jose Cano', 1, 0, 1, 1);
-
-INSERT INTO OwnedFilms VALUES(1001, 'The Dark Knight', 1080);
-INSERT INTO OwnedFilms VALUES(1001, 'The Dark Knight Rises', 1080);
-INSERT INTO OwnedFilms VALUES(1001, 'Terminator 2: Judgment Day', 2160);
-INSERT INTO OwnedFilms VALUES(1001, 'Guardians of the Galaxy Vol. 3', 720);
-
-INSERT INTO WatchLater VALUES(1001, 'Scott Pilgrim vs. the World');
-INSERT INTO WatchLater VALUES(1001, 'Kill Bill: Vol. 2');
-INSERT INTO WatchLater VALUES(1001, 'Avatar');
-
-INSERT INTO Subtitles VALUES('The Dark Knight', 'english');
-INSERT INTO Subtitles VALUES('The Dark Knight', 'spanish');
-INSERT INTO Subtitles VALUES('The Dark Knight', 'french');
-INSERT INTO Subtitles VALUES('The Dark Knight Rises', 'english');
-INSERT INTO Subtitles VALUES('Terminator 2: Judgment Day', 'russian');
-
