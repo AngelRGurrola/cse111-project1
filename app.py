f1f6f46fa8c5454206f4ed6a1076042c5a3f4d92
CanoJose92401 <110748868+CanoJose92401@users.noreply.github.com>
2023-12-04 12:57:48 -0800

    Added Watch Later and it's functionality

--- app.py
+++ app.py
@@ -18,18 +18,18 @@ def closeConnection(_conn, _dbFile):
 
 def start(_conn):
         while True:
-            print("\nMOVIE ORGANIZER: ")
+            print("\n------------------------------------------")
+            print("MOVIE ORGANIZER: ")
             print("1. Login Profile")
             print("2. Create Profile")
-            print("3. End Session")
+            print("3. End Session") 
 
-            num = input("Enter desired number here: ")
+            num = input("\nEnter desired number here: ")
 
             if num == "1":
                 login(_conn)
             elif num == "2":
                 create(_conn)
-
             elif num == "3":
                 break
             
@@ -54,7 +54,6 @@ def create(_conn):
         user_id = row[0]
     print("%d, %s, %d, %d, %d, %d" % (user_id, user_name, plat_a, plat_h, plat_n, plat_d)) #testing the caputer of User Inputs
 
-
 def changeAmazon():
     while True:
         check = input("\nAmazon Prime Y or N: ")
@@ -67,6 +66,7 @@ def changeAmazon():
             return plat_a
         else:
             print("Please enter a correct response")
+
 def changeHulu():
     while True:
         check = input("\nHULU Y or N: ")
@@ -107,7 +107,7 @@ def changeDisney():
             print("Please enter a correct response")
 
 def login(_conn):
-    print("Choose an Existing Profile")
+    print("\nChoose an Existing Profile")
     try:   
         sql = """SELECT userName FROM Users"""
         cur = _conn.cursor()
@@ -144,32 +144,137 @@ def login(_conn):
 
         loginInterface(user_name, user_id, _conn)
         
-
     except Error as e:
         print(e)
 
 def loginInterface(name, id, _conn):
     while True:
-        print("\nWELCOME %s to your Film Organizer" % (name))
+        print("\n------------------------------------------")
+        print("WELCOME %s TO YOUR FILM ORGANIZER" % (name))
         print("1. Watch Later")
         print("2. Owned Films")
         print("3. Streaming Platform")
         print("4. Log Out")
 
-        num = input("Enter desired number here: ")
+        num = input("\nEnter desired number here: ")
 
         if num == "1":
-            print("hi")
+            watchLaterInterface(name, id, _conn)
         elif num == "2":
             print("hi")
 
         elif num == "3":
             print("hi")
         elif num == "4":
-            start(_conn)
+            break
         
         else:
             print("Invalid Input: Please select a desired action with the appropriate number")
+            
+def watchLaterInterface(name, id, _conn):
+    while True:
+        print("\n------------------------------------------")
+        print("WATCH LATER LIST:")
+        statement = '''
+            SELECT * 
+            FROM WatchLater w
+            WHERE ? = w.userID;
+        '''
+        arg = [id]
+        
+        cur = _conn.cursor()
+        cur.execute(statement, arg)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row[1])
+        
+        print("\nOPTIONS:") 
+        print("1: Add to list")
+        print("2: Delete from list")
+        print("3: More details on film")
+        print("4: Return back")
+        userInput = input("\nEnter option:")
+        
+        if userInput == "1":
+            print("ADD")
+        elif userInput == "2": 
+            print("\nWHICH FILM DO YOU WANT TO DELETE?")
+            
+            i = 1
+            for row in rows:
+                print(i, ": ", row[1])
+                i += 1
+            print("0: Cancel")
+            deleteRow = input("\nSelect number ")
+            if int(deleteRow) != 0:
+                deleteFromWatchLater(id, rows[int(deleteRow) - 1][1], _conn)
+        elif userInput == "3":
+            print("\nWHICH FILM DO YOU WANT MORE DETAIL?")
+            
+            i = 1
+            for row in rows:
+                print(i, ": ", row[1])
+                i += 1
+            print("0: Cancel")
+            deleteRow = input("\nSelect number ")
+            if int(deleteRow) != 0:
+                filmDetails(rows[int(deleteRow) - 1][1], _conn)
+        elif userInput == "4":
+            break
+        else:
+            print("Invalid option")
+        
+def deleteFromWatchLater (id, filmName, _conn):
+    cur = _conn.cursor()
+    args = [id, filmName]
+    
+    statement = '''
+        DELETE FROM WatchLater
+        WHERE userID = ?
+            AND filmName = ?;
+    '''
+    
+    cur.execute(statement, args)
+    print(filmName, "HAS BEEN DELETED FROM YOUR LIST.")
+    
+    # Uncomment this to save the changes in the database
+    # cur.commit()
+    
+    return 0
+        
+def filmDetails (filmName, _conn):
+    cur = _conn.cursor()
+    arg = [filmName]
+    
+    statement = '''
+    SELECT movieTitle as Title,
+        year as Year, 
+        genre as Genre,
+        summary as Summary,
+        imdbRating as Rating,
+        director as Director,
+        star1 as Stars,
+        star2 as Stars,
+        star3 as Stars,
+        star4 as Stars
+    FROM movieDetails
+    WHERE movieTitle = ?;
+    '''
+    
+    cur.execute(statement, arg)
+    rows = cur.fetchall()
+    
+    for row in rows:
+        print(f"Movie Title: {row[0]}")
+        print(f"Year: {row[1]}")
+        print(f"Genre: {row[2]}")
+        print(f"Summary: {row[3]}")
+        print(f"imbdRating: {row[4]}")
+        print(f"Director: {row[5]}")
+        print(f"Stars: {row[6]}, {row[7]}, {row[8]}, {row[9]}")
+    
+    
+    return 0
 
 def main():
     database = r"film.sqlite"
