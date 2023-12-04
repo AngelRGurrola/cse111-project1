d3fa1ccd9444c37c87cc9199b1c88b146cbd0e2c
Angel Gurrola <angelgurrola2@gmail.com>
2023-12-03 23:12:06 -0800

    made the create function in order to capture user inputs

--- app.py
+++ app.py
@@ -16,6 +16,96 @@ def closeConnection(_conn, _dbFile):
     except Error as e:
         print(e)
 
+def start(_conn):
+        while True:
+            print("\nMOVIE ORGANIZER: ")
+            print("1. Login Profile")
+            print("2. Create Profile")
+            print("3. End Session")
+
+            num = input("Enter desired number here: ")
+
+            if num == "1":
+                login(_conn)
+            elif num == "2":
+                create(_conn)
+
+            elif num == "3":
+                break
+            
+            else:
+                print("Invalid Input: Please select a desired action with the appropriate number")
+
+def create(_conn):
+    print("\n    Creating User")
+    print("-------------------------------")
+    user_name = input("Enter User Name: ")
+    print("\nFilm Platforms: Amazon Prime / HULU / Netflix / Disney+")
+    plat_a = changeAmazon()
+    plat_h = changeHulu()
+    plat_n = changeNetflix()
+    plat_d = changeDisney()
+
+    sql = """SELECT max(userID) + 1 FROM Users"""
+    cur = _conn.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:        #DO I NEED TO ITERATE TO GO INTO THE RESULT
+        user_id = row[0]
+    print("%d, %s, %d, %d, %d, %d" % (user_id, user_name, plat_a, plat_h, plat_n, plat_d)) #testing the caputer of User Inputs
+
+
+def changeAmazon():
+    while True:
+        check = input("\nAmazon Prime Y or N: ")
+        check.upper()
+        if check == "Y":
+            plat_a = 1
+            return plat_a
+        elif check == "N":
+            plat_a == 0
+            return plat_a
+        else:
+            print("Please enter a correct response")
+def changeHulu():
+    while True:
+        check = input("\nHULU Y or N: ")
+        check.upper()
+        if check == "Y":
+            plat_h = 1
+            return plat_h
+        elif check =="N":
+            plat_h = 0
+            return plat_h
+        else:
+            print("Please enter a correct response")
+
+def changeNetflix():
+    while True:
+        check = input("\nNetflix Y or N: ")
+        check.upper()
+        if check == "Y":
+            plat_n = 1
+            return plat_n
+        elif check == "N":
+            plat_n = 0
+            return plat_n
+        else:
+            print("Please enter a correct response")
+
+def changeDisney():
+    while True:
+        check = input("\nDisney+ Y or N: ")
+        check.upper()
+        if check == "Y":
+            plat_d = 1
+            return plat_d
+        elif check == "N":
+            plat_d = 0
+            return plat_d
+        else:
+            print("Please enter a correct response")
+
 def login(_conn):
     print("Choose an Existing Profile")
     try:   
@@ -76,7 +166,7 @@ def loginInterface(name, id, _conn):
         elif num == "3":
             print("hi")
         elif num == "4":
-            print("bye")
+            start(_conn)
         
         else:
             print("Invalid Input: Please select a desired action with the appropriate number")
@@ -87,25 +177,7 @@ def main():
     # create a database connection
     conn = openConnection(database)
     
-    while True:
-        print("\nMOVIE ORGANIZER: ")
-        print("1. Login Profile")
-        print("2. Create Profile")
-        print("3. End Session")
-
-        num = input("Enter desired number here: ")
-
-        if num == "1":
-            print("hi")
-            login(conn)
-        elif num == "2":
-            print("hi")
-
-        elif num == "3":
-            break
-        
-        else:
-            print("Invalid Input: Please select a desired action with the appropriate number")
+    start(conn)
 
     closeConnection(conn, database)
 
